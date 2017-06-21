34c1d93bc6c053ddba46e3f333d1d888639ba9f5
Jason Witt <jawittdesigns@gmail.com>
2017-06-21 14:07:21 -0400

    Code cleanup

--- includes/classes/settings/enable-settings.php
+++ includes/classes/settings/enable-settings.php
@@ -9,17 +9,6 @@
  * @license    GNU General Public License v2 or later
  * @version    0.0.1
  */
-$allowed_tags = array(
-	'tr'    => array(),
-	'th'    => array(),
-	'td'    => array(),
-	'input' => array(
-		'type'    => array(),
-		'name'    => array(),
-		'checked' => array(),
-		'value'   => array(),
-	),
-);
 
 // Enable CMS Settings field.
 echo wp_kses( wpcmss_create_checkbox(
@@ -28,5 +17,5 @@ echo wp_kses( wpcmss_create_checkbox(
 		$this->plugin_slug,
 		$this->settings
 	),
-	$allowed_tags
+	$this->allowed_tags
 );
