6811ba8426b982adc9f95206a93e7130f1a00d7d
Jason Witt <jawittdesigns@gmail.com>
2017-06-21 14:19:26 -0400

    Renamed file
    Renamed file and added tab section title

--- includes/classes/settings/general.php
+++ includes/classes/settings/general.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Enable Settings
+ * General Settings
  *
  * @package    WP_CMS_Settings
  * @subpackage WP_CMS_Settings/Includes/Classes/Settings
@@ -9,7 +9,13 @@
  * @license    GNU General Public License v2 or later
  * @version    0.0.1
  */
-
+?>
+<tr>
+	<th style="padding: 0 10px 0">
+		<h2><?php echo esc_html( __( 'General Settings', 'wp-cms-settings' ) ); ?></h2>
+	</th>
+</tr>
+<?php
 // Enable CMS Settings field.
 echo wp_kses( wpcmss_create_checkbox(
 		'enable_cms_settings',
