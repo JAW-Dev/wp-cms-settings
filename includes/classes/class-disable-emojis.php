c2bed1e76259c46b2cf7a9b6e5382efbc9d01109
Jason Witt <jawittdesigns@gmail.com>
2017-06-22 07:40:22 -0400

    Add disable emojis class

---
+++ includes/classes/class-disable-emojis.php
@@ -0,0 +1,123 @@
+<?php
+/**
+ * Disable Emojis
+ *
+ * @package    WP_CMS_Settings
+ * @subpackage WP_CMS_Settings/Includes/Classes
+ * @author     Jason Witt <contact@jawittdesigns.com>
+ * @copyright  Copyright (c) 2017, Jason Witt
+ * @license    GNU General Public License v2 or later
+ * @version    0.0.1
+ */
+
+namespace WP_CMS_Settings\Includes\Classes;
+
+use \WP_CMS_Settings as Root;
+
+if ( ! class_exists( 'Disable_Emojis' ) ) {
+
+	/**
+	 * Name
+	 *
+	 * @author Jason Witt
+	 * @since  0.0.1
+	 */
+	class Disable_Emojis {
+
+		/**
+		 * Settings.
+		 *
+		 * @author Jason Witt
+		 * @since  0.0.1
+		 *
+		 * @var array
+		 */
+		protected $settings;
+
+		/**
+		 * Initialize the class
+		 *
+		 * @author Jason Witt
+		 * @since  0.0.1
+		 *
+		 * @return void
+		 */
+		public function __construct() {
+			// Set the properties.
+			$this->settings = Root\wp_cms_settings()->get_settings;
+			$this->init();
+		}
+
+		/**
+		 * Init
+		 *
+		 * @author Jason Witt
+		 * @since  0.0.1
+		 *
+		 * @return void
+		 */
+		public function init() {
+			$option = $this->settings['disable_emojis'];
+			if ( isset( $option ) && $option ) {
+				add_action( 'init', array( $this, 'disable_emojis' ) );
+			}
+		}
+
+		/**
+		 * Disable Emojis.
+		 *
+		 * @author Jason Witt
+		 * @since  0.0.1
+		 *
+		 * @return void
+		 */
+		public function disable_emojis() {
+			remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+			remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+			remove_action( 'wp_print_styles', 'print_emoji_styles' );
+			remove_action( 'admin_print_styles', 'print_emoji_styles' );
+			remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+			remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+			remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+			add_filter( 'tiny_mce_plugins', array( $this, 'disable_emojis_tinymce' ) );
+			add_filter( 'wp_resource_hints', array( $this, 'disable_emojis_remove_dns_prefetch' ), 10, 2 );
+		}
+
+		/**
+		 * Disable TinyMCE emojis plugin.
+		 *
+		 * @author Jason Witt
+		 * @since  0.0.1
+		 *
+		 * @param array $plugins An array of plugins.
+		 *
+		 * @return array
+		 */
+		public function disable_emojis_tinymce( $plugins ) {
+			if ( is_array( $plugins ) ) {
+				return array_diff( $plugins, array( 'wpemoji' ) );
+			} else {
+				return array();
+			}
+		}
+
+		/**
+		 * Disable emoji DNS prefetching.
+		 *
+		 * @author Jason Witt
+		 * @since  0.0.1
+		 *
+		 * @param array  $urls          URLs to print for resource hints.
+		 * @param string $relation_type The relation type the URLs are printed for.
+		 *
+		 * @return array
+		 */
+		public function disable_emojis_remove_dns_prefetch( $urls, $relation_type ) {
+			if ( 'dns-prefetch' === $relation_type ) {
+				$emoji_svg_url = apply_filters( 'emoji_svg_url', 'https://s.w.org/images/core/emoji/2.2.1/svg/' );
+				$urls          = array_diff( $urls, array( $emoji_svg_url ) );
+			}
+			return $urls;
+		}
+	}
+}
