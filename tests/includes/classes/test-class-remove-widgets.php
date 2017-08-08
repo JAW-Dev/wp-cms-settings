9d4889d2e20f6f887cfedbdc0a0b9de792af8958
Jason Witt <jawittdesigns@gmail.com>
2017-08-08 08:09:23 -0400

    Updated test

--- tests/includes/classes/test-class-remove-widgets.php
+++ tests/includes/classes/test-class-remove-widgets.php
@@ -30,32 +30,6 @@
 		 $this->file            = $this->dirname() . 'includes/classes/class-remove-widgets.php';
 		 $this->class_name      = 'WP_CMS_Settings\\Includes\\Classes\\Remove_Widgets';
 		 $this->class           = new WP_CMS_Settings\Includes\Classes\Remove_Widgets();
-		 $this->widgets         = array(
-			 'remove_widget_pages',
-			 'remove_widget_calendar',
-			 'remove_widget_archives',
-			 'remove_widget_media_audio',
-			 'remove_widget_media_image',
-			 'remove_widget_media_video',
-			 'remove_widget_meta',
-			 'remove_widget_search',
-			 'remove_widget_text',
-			 'remove_widget_categories',
-			 'remove_widget_recent_posts',
-			 'remove_widget_recent_comments',
-			 'remove_widget_rss',
-			 'remove_widget_tag_cloud',
-			 'remove_nav_menu_widget',
-			 'remove_widget_custom_html',
-		 );
-		 $this->methods         = array(
-			 'init',
-		 );
-		 $this->widgets         = array_merge( $this->widgets, $this->widgets );
-		 $this->properties      = array(
-			 'settings',
-			 'widgets',
-		 );
 		 $this->option_settings = array(
 			 array(
 				 'name' => 'remove_widget_pages',
@@ -77,28 +51,5 @@
 			 ),
 		 );
 		 $this->set_the_options();
-	 }
-
-	 /**
-	  * Test Init.
-	  *
-	  * @author Jason Witt
-	  * @since  0.0.1
-	  *
-	  * @return void
-	  */
-	 public function test_init() {
-		 $this->class->init();
-		 $hooks = array();
-
-		 foreach ( $this->widgets as $widget ) {
-			 $hooks[] = array(
-				 'hook_name' => 'widgets_init',
-				 'type'      => 'add_action',
-				 'method'    => $widget,
-				 'priority'  => 11,
-			 );
-		 }
-		 $this->assertAddHooks( $hooks );
 	 }
  }
