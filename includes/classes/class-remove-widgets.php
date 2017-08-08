cc051b63edad921f97ceae27f3211db790d1e9af
Jason Witt <jawittdesigns@gmail.com>
2017-08-08 07:17:02 -0400

    Refactored remove widgets

--- includes/classes/class-remove-widgets.php
+++ includes/classes/class-remove-widgets.php
@@ -35,16 +35,6 @@ if ( ! class_exists( 'Remove_Widgets' ) ) {
 		protected $settings;
 
 		/**
-		 * Widgets.
-		 *
-		 * @author Jason Witt
-		 * @since  0.0.1
-		 *
-		 * @var array
-		 */
-		protected $widgets;
-
-		/**
 		 * Initialize the class
 		 *
 		 * @author Jason Witt
@@ -53,25 +43,7 @@ if ( ! class_exists( 'Remove_Widgets' ) ) {
 		 * @return void
 		 */
 		public function __construct() {
-			// Set the properties.
 			$this->settings = Root\wp_cms_settings()->get_settings;
-			$this->widgets = array(
-				'remove_widget_pages',
-				'remove_widget_calendar',
-				'remove_widget_archives',
-				'remove_widget_media_audio',
-				'remove_widget_media_image',
-				'remove_widget_media_video',
-				'remove_widget_meta',
-				'remove_widget_search',
-				'remove_widget_text',
-				'remove_widget_categories',
-				'remove_widget_recent_posts',
-				'remove_widget_recent_comments',
-				'remove_widget_rss',
-				'remove_widget_tag_cloud',
-				'remove_widget_nav_menu',
-			);
 			$this->init();
 		}
 
@@ -84,192 +56,72 @@ if ( ! class_exists( 'Remove_Widgets' ) ) {
 		 * @return void
 		 */
 		public function init() {
-			foreach ( $this->widgets as $widget ) {
-				$option = ( isset( $this->settings[ $widget ] ) ) ? $this->settings[ $widget ] : 'false';
-				if ( 'true' === $option ) {
-					add_action( 'widgets_init', array( $this, $widget ),11 );
+			add_action( 'widgets_init', array( $this, 'unregister_widgets' ),11 );
+		}
+		
+		/**
+		 * Get WP Widgets.
+		 *
+		 * @author Jason Witt
+		 * @since  0.0.1
+		 *
+		 * @return void
+		 */
+		public function get_wp_widgets( $callback ) {
+			global $wp_widget_factory;
+			
+			// Loop through the registeres widgets.
+			foreach ( $wp_widget_factory->widgets as $widget ) {
+				$class_name =  get_class( $widget );
+				
+				// If the widget is a core widget.
+				// Test by looking for "WP_" prefix on object name.
+				if ( strpos( $class_name, 'WP_' ) !== false ) {
+					$option = str_replace( 'wp', 'remove', strtolower( $class_name ) );
+					call_user_func_array( $callback, array( $class_name, $option, $widget ) );
 				}
 			}
 		}
-
+		
 		/**
-		 * Remove WP_Widget_Pages.
+		 * Unregister Widgets.
 		 *
 		 * @author Jason Witt
 		 * @since  0.0.1
 		 *
 		 * @return void
 		 */
-		public function remove_widget_pages() {
-			unregister_widget( 'WP_Widget_Pages' );
+		public function unregister_widgets() {
+			$this->get_wp_widgets( function( $class_name, $option, $widget ) {
+				$setting     = ( isset( $this->settings[ $option ] ) ) ? $this->settings[ $option ] : 'false';
+				if ( 'true' === $setting ) {
+					unregister_widget( $class_name );
+				}
+			});
 		}
-
+		
 		/**
-		 * Remove WP_Widget_Calendar.
+		 * Render Checkboxes.
 		 *
 		 * @author Jason Witt
 		 * @since  0.0.1
 		 *
 		 * @return void
 		 */
-		public function remove_widget_calendar() {
-			unregister_widget( 'WP_Widget_Calendar' );
-		}
-
-		/**
-		 * Remove WP_Widget_Archives.
-		 *
-		 * @author Jason Witt
-		 * @since  0.0.1
-		 *
-		 * @return void
-		 */
-		public function remove_widget_archives() {
-			unregister_widget( 'WP_Widget_Archives' );
-		}
-
-		/**
-		 * Remove WP_Widget_Media_Audio.
-		 *
-		 * @author Jason Witt
-		 * @since  0.0.1
-		 *
-		 * @return void
-		 */
-		public function remove_widget_media_audio() {
-			unregister_widget( 'WP_Widget_Media_Audio' );
-		}
-
-		/**
-		 * Remove WP_Widget_Media_Image.
-		 *
-		 * @author Jason Witt
-		 * @since  0.0.1
-		 *
-		 * @return void
-		 */
-		public function remove_widget_media_image() {
-			unregister_widget( 'WP_Widget_Media_Image' );
-		}
-
-		/**
-		 * Remove WP_Widget_Media_Video.
-		 *
-		 * @author Jason Witt
-		 * @since  0.0.1
-		 *
-		 * @return void
-		 */
-		public function remove_widget_media_video() {
-			unregister_widget( 'WP_Widget_Media_Video' );
-		}
-
-		/**
-		 * Remove WP_Widget_Meta.
-		 *
-		 * @author Jason Witt
-		 * @since  0.0.1
-		 *
-		 * @return void
-		 */
-		public function remove_widget_meta() {
-			unregister_widget( 'WP_Widget_Meta' );
-		}
-
-		/**
-		 * Remove WP_Widget_Search.
-		 *
-		 * @author Jason Witt
-		 * @since  0.0.1
-		 *
-		 * @return void
-		 */
-		public function remove_widget_search() {
-			unregister_widget( 'WP_Widget_Search' );
-		}
-
-		/**
-		 * Remove WP_Widget_Text.
-		 *
-		 * @author Jason Witt
-		 * @since  0.0.1
-		 *
-		 * @return void
-		 */
-		public function remove_widget_text() {
-			unregister_widget( 'WP_Widget_Text' );
-		}
-
-		/**
-		 * Remove WP_Widget_Categories.
-		 *
-		 * @author Jason Witt
-		 * @since  0.0.1
-		 *
-		 * @return void
-		 */
-		public function remove_widget_categories() {
-			unregister_widget( 'WP_Widget_Categories' );
-		}
-
-		/**
-		 * Remove WP_Widget_Recent_Posts.
-		 *
-		 * @author Jason Witt
-		 * @since  0.0.1
-		 *
-		 * @return void
-		 */
-		public function remove_widget_recent_posts() {
-			unregister_widget( 'WP_Widget_Recent_Posts' );
-		}
-
-		/**
-		 * Remove WP_Widget_Recent_Comments.
-		 *
-		 * @author Jason Witt
-		 * @since  0.0.1
-		 *
-		 * @return void
-		 */
-		public function remove_widget_recent_comments() {
-			unregister_widget( 'WP_Widget_Recent_Comments' );
-		}
-
-		/**
-		 * Remove WP_Widget_RSS.
-		 *
-		 * @author Jason Witt
-		 * @since  0.0.1
-		 *
-		 * @return void
-		 */
-		public function remove_widget_rss() {
-			unregister_widget( 'WP_Widget_RSS' );
-		}
-
-		/**
-		 * Remove WP_Widget_Tag_Cloud.
-		 *
-		 * @author Jason Witt
-		 * @since  0.0.1
-		 *
-		 * @return void
-		 */
-		public function remove_widget_tag_cloud() {
-			unregister_widget( 'WP_Widget_Tag_Cloud' );
-		}
-
-		/**
-		 * Remove WP_Nav_Menu_Widget.
-		 *
-		 * @author Jason Witt
-		 * @since  0.0.1
-		 *
-		 * @return void
-		 */
-		public function remove_widget_nav_menu() {
-			unregister_widget( 'WP_Nav_Menu_Widget' );
+		public function render_checkboxes() {
+			$this->get_wp_widgets(
+				function( $class_name, $option, $widget ) {
+					echo wp_kses_post(
+						wpcmss_create_checkbox(
+							array(
+								'option' => $option,
+								'label'  => __( $widget->name, 'wp-cms-settings' ),
+								'description' => __( "Remove $widget->name from the widgets list", 'wp-cms-settings' ),
+							)
+						)
+					);
+				}
+			);
 		}
 	}
 }
--- includes/classes/settings/widgets.php
+++ includes/classes/settings/widgets.php
@@ -22,104 +22,111 @@
 echo wp_kses_post( wpcmss_create_checkbox( array(
 		'option' => 'remove_widget_pages',
 		'label'  => __( 'Pages Widget', 'wp-cms-settings' ),
-		'description' => __( 'Remove the Pages from the widgets list', 'wp-cms-settings' ),
+		'description' => __( 'Remove Pages from the widgets list', 'wp-cms-settings' ),
 	) )
 );
 // Disable WP_Widget_Calendar.
 echo wp_kses_post( wpcmss_create_checkbox( array(
 		'option' => 'remove_widget_calendar',
 		'label'  => __( 'Calendar Widget', 'wp-cms-settings' ),
-		'description' => __( 'Remove the Calendar from the widgets list', 'wp-cms-settings' ),
+		'description' => __( 'Remove Calendar from the widgets list', 'wp-cms-settings' ),
 	) )
 );
 // Disable WP_Widget_Archives.
 echo wp_kses_post( wpcmss_create_checkbox( array(
 		'option' => 'remove_widget_archives',
 		'label'  => __( 'Archives Widget', 'wp-cms-settings' ),
-		'description' => __( 'Remove the Archives from the widgets list', 'wp-cms-settings' ),
+		'description' => __( 'Remove Archives from the widgets list', 'wp-cms-settings' ),
 	) )
 );
 // Disable WP_Widget_Media_Audio.
 echo wp_kses_post( wpcmss_create_checkbox( array(
 		'option' => 'remove_widget_media_audio',
 		'label'  => __( 'Media Audio Widget', 'wp-cms-settings' ),
-		'description' => __( 'Remove the Media Audio from the widgets list', 'wp-cms-settings' ),
+		'description' => __( 'Remove Media Audio from the widgets list', 'wp-cms-settings' ),
 	) )
 );
 // Disable WP_Widget_Media_Image.
 echo wp_kses_post( wpcmss_create_checkbox( array(
 		'option' => 'remove_widget_media_image',
 		'label'  => __( 'Media Image Widget', 'wp-cms-settings' ),
-		'description' => __( 'Remove the Media Image from the widgets list', 'wp-cms-settings' ),
+		'description' => __( 'Remove Media Image from the widgets list', 'wp-cms-settings' ),
 	) )
 );
 // Disable WP_Widget_Media_Video.
 echo wp_kses_post( wpcmss_create_checkbox( array(
 		'option' => 'remove_widget_media_video',
 		'label'  => __( 'Media Video Widget', 'wp-cms-settings' ),
-		'description' => __( 'Remove the Media Video from the widgets list', 'wp-cms-settings' ),
+		'description' => __( 'Remove Media Video from the widgets list', 'wp-cms-settings' ),
 	) )
 );
 // Disable WP_Widget_Meta.
 echo wp_kses_post( wpcmss_create_checkbox( array(
 		'option' => 'remove_widget_meta',
 		'label'  => __( 'Meta Widget', 'wp-cms-settings' ),
-		'description' => __( 'Remove the Meta from the widgets list', 'wp-cms-settings' ),
+		'description' => __( 'Remove Meta from the widgets list', 'wp-cms-settings' ),
 	) )
 );
 // Disable WP_Widget_Search.
 echo wp_kses_post( wpcmss_create_checkbox( array(
 		'option' => 'remove_widget_search',
 		'label'  => __( 'Search Widget', 'wp-cms-settings' ),
-		'description' => __( 'Remove the Search from the widgets list', 'wp-cms-settings' ),
+		'description' => __( 'Remove Search from the widgets list', 'wp-cms-settings' ),
 	) )
 );
 // Disable WP_Widget_Text.
 echo wp_kses_post( wpcmss_create_checkbox( array(
 		'option' => 'remove_widget_text',
 		'label'  => __( 'Text Widget', 'wp-cms-settings' ),
-		'description' => __( 'Remove the Text from the widgets list', 'wp-cms-settings' ),
+		'description' => __( 'Remove Text from the widgets list', 'wp-cms-settings' ),
 	) )
 );
 // Disable WP_Widget_Categories.
 echo wp_kses_post( wpcmss_create_checkbox( array(
 		'option' => 'remove_widget_categories',
 		'label'  => __( 'Categories Widget', 'wp-cms-settings' ),
-		'description' => __( 'Remove the Categories from the widgets list', 'wp-cms-settings' ),
+		'description' => __( 'Remove Categories from the widgets list', 'wp-cms-settings' ),
 	) )
 );
 // Disable WP_Widget_Recent_Posts.
 echo wp_kses_post( wpcmss_create_checkbox( array(
 		'option' => 'remove_widget_recent_posts',
 		'label'  => __( 'Recent Posts Widget', 'wp-cms-settings' ),
-		'description' => __( 'Remove the Recent Posts from the widgets list', 'wp-cms-settings' ),
+		'description' => __( 'Remove Recent Posts from the widgets list', 'wp-cms-settings' ),
 	) )
 );
 // Disable WP_Widget_Recent_Comments.
 echo wp_kses_post( wpcmss_create_checkbox( array(
 		'option' => 'remove_widget_recent_comments',
 		'label'  => __( 'Recent Comments Widget', 'wp-cms-settings' ),
-		'description' => __( 'Remove the Recent Comments from the widgets list', 'wp-cms-settings' ),
+		'description' => __( 'Remove Recent Comments from the widgets list', 'wp-cms-settings' ),
 	) )
 );
 // Disable WP_Widget_RSS.
 echo wp_kses_post( wpcmss_create_checkbox( array(
 		'option' => 'remove_widget_rss',
 		'label'  => __( 'RSS Widget', 'wp-cms-settings' ),
-		'description' => __( 'Remove the RSS from the widgets list', 'wp-cms-settings' ),
+		'description' => __( 'Remove RSS from the widgets list', 'wp-cms-settings' ),
 	) )
 );
 // Disable WP_Widget_Tag_Cloud.
 echo wp_kses_post( wpcmss_create_checkbox( array(
 		'option' => 'remove_widget_tag_cloud',
 		'label'  => __( 'Tag Cloud Widget', 'wp-cms-settings' ),
-		'description' => __( 'Remove the Tag Cloud from the widgets list', 'wp-cms-settings' ),
+		'description' => __( 'Remove Tag Cloud from the widgets list', 'wp-cms-settings' ),
 	) )
 );
 // Disable WP_Nav_Menu_Widget.
 echo wp_kses_post( wpcmss_create_checkbox( array(
-		'option' => 'remove_widget_nav_menu',
+		'option' => 'remove_nav_menu_widget',
 		'label'  => __( 'Nav Menu Widget', 'wp-cms-settings' ),
-		'description' => __( 'Remove the Nav Menu from the widgets list', 'wp-cms-settings' ),
+		'description' => __( 'Remove Nav Menu from the widgets list', 'wp-cms-settings' ),
+	) )
+);
+// Disable WP_Custom_HTML_Widget.
+echo wp_kses_post( wpcmss_create_checkbox( array(
+		'option' => 'remove_widget_custom_html',
+		'label'  => __( 'Custom HTML Widget', 'wp-cms-settings' ),
+		'description' => __( 'Remove Custom HTML from the widgets list', 'wp-cms-settings' ),
 	) )
 );
