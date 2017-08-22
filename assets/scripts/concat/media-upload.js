952e20197c2af30a935348a143583b5c5745d0b0
Jason Witt <jawittdesigns@gmail.com>
2017-08-22 18:09:24 -0400

    Refactored

--- assets/scripts/concat/media-upload.js
+++ assets/scripts/concat/media-upload.js
@@ -1,15 +1,34 @@
 /**
  * Media Upload
+ *
+ * @author Jason Witt
+ * @since 0.0.1
  */
-window.mediaUploader = {};
+window.siteIcon = {};
 ( function( window, app ) {
 
+	/**
+	 * Initlize.
+	 *
+	 * @author Jason Witt
+	 * @since 0.0.1
+	 *
+	 * @return void
+	 */
 	app.init = function() {
 		app.cache();
 		app.chooseImage();
-		app.deleteImage();
+		app.removeImage();
 	};
 
+	/**
+	 * Cache the DOM elements.
+	 *
+	 * @author Jason Witt
+	 * @since 0.0.1
+	 *
+	 * @return void
+	 */
 	app.cache = function() {
 		app.$c = {
 			uploadButton: document.getElementById( 'upload-button' ),
@@ -18,22 +37,32 @@ window.mediaUploader = {};
 			browserPreview: document.getElementById( 'browser-preview' ),
 			appIconPreview: document.getElementById( 'app-icon-preview' ),
 			siteIcon: document.getElementById( 'site_icon' ),
+			mediaUploader: false,
 		};
 	};
 
+	/**
+	 * Choose Image.
+	 *
+	 * @author Jason Witt
+	 * @since 0.0.1
+	 *
+	 * @return void
+	 */
 	app.chooseImage = function() {
-		var mediaUploader;
 
 		app.$c.uploadButton.addEventListener( 'click', function( e ) {
 
 			e.preventDefault();
 
-			if ( mediaUploader ) {
-				mediaUploader.open();
+			// If media uploader exists, reopen it.
+			if ( app.$c.mediaUploader ) {
+				app.$c.mediaUploader.open();
 				return;
 			}
 
-			mediaUploader = wp.media({
+			// Create the media uploader frame.
+			app.$c.mediaUploader = wp.media({
 				title: 'Choose Image',
 				button: {
 					text: 'Select',
@@ -44,32 +73,81 @@ window.mediaUploader = {};
 				multiple: false,
 			});
 
-			// When a file is selected, grab the URL and set it as the text field's value
-			mediaUploader.on( 'select', function() {
-				var attachment = mediaUploader.state().get( 'selection' ).first().toJSON();
-				app.$c.siteIcon.value = attachment.id;
-				app.$c.attachmentMediaView.classList.remove( 'no-show-preview' );
-				app.$c.browserPreview.setAttribute( 'src', attachment.url );
-				app.$c.appIconPreview.setAttribute( 'src', attachment.url );
-				app.$c.uploadButton.textContent = 'Change Image';
-			});
+			app.onSelect();
 
 			// Open the uploader dialog
-			mediaUploader.open();
+			app.$c.mediaUploader.open();
 		});
 	};
 
-	app.deleteImage = function() {
+	/**
+	 * Select Image.
+	 *
+	 * @author Jason Witt
+	 * @since 0.0.1
+	 *
+	 * @return void
+	 */
+	app.onSelect = function() {
+
+		// When a image is selected.
+		app.$c.mediaUploader.on( 'select', function() {
+
+			// Get the attachment details.
+			var attachment = app.$c.mediaUploader.state().get( 'selection' ).first().toJSON();
+
+			// Set the value on the site_icon field.
+			app.$c.siteIcon.value = attachment.id;
+
+			// Remove the 'no-show-preview' class if present.
+			app.$c.attachmentMediaView.classList.remove( 'no-show-preview' );
+
+			// Set the URL to the image src attributes.
+			app.$c.browserPreview.setAttribute( 'src', attachment.url );
+			app.$c.appIconPreview.setAttribute( 'src', attachment.url );
+
+			// Change the button text.
+			app.$c.uploadButton.textContent = 'Change Image';
+		});
+	};
+
+	/**
+	 * Remove Image.
+	 *
+	 * @author Jason Witt
+	 * @since 0.0.1
+	 *
+	 * @return void
+	 */
+	app.removeImage = function() {
 		app.$c.removeButton.addEventListener( 'click', function() {
+
+			// Set the conformation modal.
 			var answer = confirm( 'Are you sure?' );
+
+			// If confirmed.
 			if ( answer == true ) {
+
+				// Set the site_icon field to 0.
 				app.$c.siteIcon.value = '0';
+
+				// Add the 'no-show-preview' class.
 				app.$c.attachmentMediaView.classList.add( 'no-show-preview' );
+
+				// Change the button text.
 				app.$c.uploadButton.textContent = 'Select Image';
 			}
 			return false;
 		});
 	};
 
+	/**
+	 * Run the script.
+	 *
+	 * @author Jason Witt
+	 * @since 0.0.1
+	 *
+	 * @return void
+	 */
 	app.init();
-}( window, window.mediaUploader ) );
+}( window, window.siteIcon ) );
